7cce084a44ffbd330f8ec63f1131621468b1638c
Stéphane <stephane@ovensia.fr>
2008-06-16 15:46:05 +0000

    Mise à jour du module WebEdit :  * Ajout des classes manquantes

---
+++ install/webedit/files/class_tag.php
@@ -0,0 +1,62 @@
+<?php
+/*
+    Copyright (c) 2008 Ovensia
+    Contributors hold Copyright (c) to their code submissions.
+
+    This file is part of Ploopi.
+
+    Ploopi is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Ploopi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ploopi; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * Gestion des étiquettes associées à un article
+ *
+ * @package webedit
+ * @subpackage tag
+ * @copyright Ovensia
+ * @license GNU General Public License (GPL)
+ * @author Stéphane Escaich
+ */
+
+/**
+ * Inclusion de la classe parent.
+ */
+
+include_once './include/classes/data_object.php';
+
+/**
+ * Classe d'accès à la table ploopi_mod_webedit_tag
+ *
+ * @package webedit
+ * @subpackage tag
+ * @copyright Ovensia
+ * @license GNU General Public License (GPL)
+ * @author Stéphane Escaich
+ */
+
+class webedit_tag extends data_object
+{
+    /**
+     * Contructeur de la classe
+     *
+     * @return webedit_tag
+     */
+    
+    function webedit_tag()
+    {
+        parent::data_object('ploopi_mod_webedit_tag', 'id');
+    } 
+}
+?>
