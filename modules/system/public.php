09edf95f2f07c225a485e0037a23b8b18674fb79
Stéphane <stephane@ovensia.fr>
2008-07-17 15:19:33 +0000

    Mise à jour du fichier public.php du module système pour gérer de manière plus souple l'accès aux différentes fonctions quelque soit l'espace sélectionné.

--- modules/system/public.php
+++ modules/system/public.php
@@ -38,98 +38,73 @@ ploopi_init_module('system');
 
 $op = (empty($_REQUEST['op'])) ? '' : $_REQUEST['op'];
 
-switch($_SESSION['ploopi']['mainmenu'])
+switch($op)
 {
-    case _PLOOPI_MENU_WORKSPACES:
-        switch($op)
+    case 'search':
+    default:
+        include_once 'public_search.php';
+    break;
+    case 'tickets':
+        include './modules/system/public_tickets.php';
+    break;
+    
+    case 'annotation':
+        include './modules/system/public_annotations.php';
+    break;
+    
+    case 'paramsave':
+        if (!empty($_POST['idmodule']) && is_numeric($_POST['idmodule']))
         {
-            case 'search':
-            default:
-                include_once 'public_search.php';
-            break;
+            $param_module = new param();
+            $param_module->open($_POST['idmodule'],0,$_SESSION['ploopi']['userid'], 1);
+            $param_module->setvalues($_POST);
+            $param_module->save();
+
+            // reload all module params of current user in session
+            include './include/load_param.php';
+            ploopi_redirect("admin.php?op=param&idmodule={$_POST['idmodule']}");
         }
+        else ploopi_redirect('admin.php');
     break;
 
-    /*
-    case _PLOOPI_MENU_TICKETS:
-        include_once 'public_tickets.php';
+    case 'param':
+        echo $skin->create_pagetitle(_PLOOPI_LABEL_MYPARAMS);
+        include './modules/system/public_module_param.php';
     break;
 
-    case _PLOOPI_MENU_ANNOTATIONS:
-        include_once 'public_annotation.php';
+    case 'actions':
+        include './modules/system/public_actions.php';
     break;
-*/
-    case _PLOOPI_MENU_MYWORKSPACE:
 
-        switch($op)
+    case 'save_user':
+        $user = new user();
+        $user->open($_SESSION['ploopi']['userid']);
+        
+        if (!isset($_POST['user_ticketsbyemail'])) $user->fields['ticketsbyemail'] = 0;
+        if (!isset($_POST['user_servertimezone'])) $user->fields['servertimezone'] = 0;
+        
+        $user->setvalues($_POST,'user_');
+
+        // affectation nouveau password
+        $passwordok = true;
+        if (isset($_POST['usernewpass']) && isset($_POST['usernewpass_confirm']))
         {
-            case 'tickets':
-                include './modules/system/public_tickets.php';
-            break;
-            
-            case 'annotation':
-                include './modules/system/public_annotations.php';
-            break;
-            
-            case 'paramsave':
-                if (!empty($_POST['idmodule']) && is_numeric($_POST['idmodule']))
-                {
-                    $param_module = new param();
-                    $param_module->open($_POST['idmodule'],0,$_SESSION['ploopi']['userid'], 1);
-                    $param_module->setvalues($_POST);
-                    $param_module->save();
-
-                    // reload all module params of current user in session
-                    include './include/load_param.php';
-                    ploopi_redirect("admin.php?op=param&idmodule={$_POST['idmodule']}");
-                }
-                else ploopi_redirect('admin.php');
-            break;
-
-            case 'param':
-                echo $skin->create_pagetitle(_PLOOPI_LABEL_MYPARAMS);
-                include './modules/system/public_module_param.php';
-            break;
-
-            case 'actions':
-                include './modules/system/public_actions.php';
-            break;
-
-            case 'save_user':
-                $user = new user();
-                $user->open($_SESSION['ploopi']['userid']);
-                
-                if (!isset($_POST['user_ticketsbyemail'])) $user->fields['ticketsbyemail'] = 0;
-                if (!isset($_POST['user_servertimezone'])) $user->fields['servertimezone'] = 0;
-                
-                $user->setvalues($_POST,'user_');
-
-                // affectation nouveau password
-                $passwordok = true;
-                if (isset($_POST['usernewpass']) && isset($_POST['usernewpass_confirm']))
-                {
-                    if ($_POST['usernewpass']!='' && $_POST['usernewpass'] == $_POST['usernewpass_confirm'])
-                    {
-                        $user->fields['password'] = md5(_PLOOPI_SECRETKEY."/{$user->fields['login']}/".md5($_POST['usernewpass']));
-                        if ($_SESSION['ploopi']['modules'][_PLOOPI_MODULE_SYSTEM]['system_generate_htpasswd']) system_generate_htpasswd($user->fields['login'], $_POST['usernewpass']);
-                    }
-                    elseif ($_POST['usernewpass'] != $_POST['usernewpass_confirm']) $passwordok = false;
-                }
-
-                $user->save();
-                if ($passwordok) ploopi_redirect("admin.php?op=profile&reloadsession");
-                else ploopi_redirect("admin.php?op=profile&error=password");
-            break;
-
-            case 'profile':
-                
-                echo $skin->create_pagetitle(_SYSTEM_LABEL_MYPROFILE);
-                
-                
-                include './modules/system/public_user.php';
-            break;
-
+            if ($_POST['usernewpass']!='' && $_POST['usernewpass'] == $_POST['usernewpass_confirm'])
+            {
+                $user->fields['password'] = md5(_PLOOPI_SECRETKEY."/{$user->fields['login']}/".md5($_POST['usernewpass']));
+                if ($_SESSION['ploopi']['modules'][_PLOOPI_MODULE_SYSTEM]['system_generate_htpasswd']) system_generate_htpasswd($user->fields['login'], $_POST['usernewpass']);
+            }
+            elseif ($_POST['usernewpass'] != $_POST['usernewpass_confirm']) $passwordok = false;
         }
+
+        $user->save();
+        if ($passwordok) ploopi_redirect("admin.php?op=profile&reloadsession");
+        else ploopi_redirect("admin.php?op=profile&error=password");
+    break;
+
+    case 'profile':
+        echo $skin->create_pagetitle(_SYSTEM_LABEL_MYPROFILE);
+        include './modules/system/public_user.php';
     break;
 }
 ?>
