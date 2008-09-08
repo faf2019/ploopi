5ee004279cf04d8af32c9a51fa6a697f890fb929
Stéphane <stephane@ovensia.fr>
2008-09-08 06:47:27 +0000

    WebEdit : mise à jour du fichier SQL d'install

--- install/webedit/structure.sql
+++ install/webedit/structure.sql
@@ -157,3 +157,8 @@ CREATE TABLE `ploopi_mod_webedit_tag` (
   KEY `id_module` (`id_module`),
   KEY `tag` (`tag`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+ALTER TABLE `ploopi_mod_webedit_article_draft` ADD `disabledfilter` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `ploopi_mod_webedit_article` ADD `disabledfilter` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `ploopi_mod_webedit_article_draft` ADD `headcontent` LONGTEXT NOT NULL;
+ALTER TABLE `ploopi_mod_webedit_article` ADD `headcontent` LONGTEXT NOT NULL;
