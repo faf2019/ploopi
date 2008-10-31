3658aeaf6fcfe262f39855f4fd271f6299b55528
Stéphane <stephane@ovensia.fr>
2008-10-31 11:14:12 +0000

    * Espaces de travail / Groupes d'utilisateurs : Correction d'un problème de rattachement pour les utilisateurs attachés à aucun groupe.

--- modules/system/admin_index_users_attachlist.php
+++ modules/system/admin_index_users_attachlist.php
@@ -1,25 +1,25 @@
 <?php
 /*
-    Copyright (c) 2002-2007 Netlor
-    Copyright (c) 2007-2008 Ovensia
-    Contributors hold Copyright (c) to their code submissions.
+ Copyright (c) 2002-2007 Netlor
+ Copyright (c) 2007-2008 Ovensia
+ Contributors hold Copyright (c) to their code submissions.
 
-    This file is part of Ploopi.
+ This file is part of Ploopi.
 
-    Ploopi is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+ Ploopi is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
 
-    Ploopi is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ Ploopi is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Ploopi; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ You should have received a copy of the GNU General Public License
+ along with Ploopi; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 /**
  * Affichage des utilisateurs "rattachables" à l'espace de travail courant 
@@ -39,37 +39,40 @@
 if (isset($_POST['reset'])) $pattern = '';
 else $pattern = (empty($_POST['pattern'])) ? '' : $_POST['pattern'];
 
+// construction du tableau de filtrage pour recherche les utilisateurs
+$arrWhere = array();
+
+switch ($_SESSION['system']['level'])
+{
+    case _SYSTEM_GROUPS :
+        // filtrage sur les groupes partagés
+        if (!empty($groups['list'][$groupid]['groups_shared'])) $arrWhere[] = '( gu.id_group IN ('.implode(',',array_keys($groups['list'][$groupid]['groups_shared'])).') OR isnull(gu.id_group))';
+        else $arrWhere[] = 'gu.id_group = 0';
+
+        $currentusers = $group->getusers();
+        if (!empty($currentusers)) $arrWhere[] = 'u.id NOT IN ('.implode(',',array_keys($currentusers)).')';
+    break;
+
+    case _SYSTEM_WORKSPACES :
+        // filtrage sur les groupes partagés
+        if (!empty($workspaces['list'][$workspaceid]['groups_shared'])) $arrWhere[] = 'gu.id_group IN ('.implode(',',array_keys($workspaces['list'][$workspaceid]['groups_shared'])).')';
+        else $arrWhere[] = "gu.id_group = 0";
+
+        $currentusers = $workspace->getusers();
+        if (!empty($currentusers)) $arrWhere[] = 'u.id NOT IN ('.implode(',',array_keys($currentusers)).')';
+    break;
+}
+
+
 if ($pattern != '') $alphaTabItem = 99; // tous
 else
 {
+    // aucun caractère de filtrage sélectionné. On recherche si on en met un par défaut (si trop d'utilisateurs) ou si on sélectionne "tous"
+    
     $alphaTabItem = (empty($_GET['alphaTabItem'])) ? -1 : $_GET['alphaTabItem'];
 
     if ($alphaTabItem == -1)
     {
-        // aucun caractère de filtrage sélectionné. On recherche si on en met un par défaut (si trop d'utilisateurs) ou si on sélectionne "tous"
-        $where = array();
-
-        switch ($_SESSION['system']['level'])
-        {
-            case _SYSTEM_GROUPS :
-                // filtrage sur les groupes partagés
-                if (!empty($groups['list'][$groupid]['groups_shared'])) $where[] = '( gu.id_group IN ('.implode(',',array_keys($groups['list'][$groupid]['groups_shared'])).') OR isnull(gu.id_group))';
-                else $where[] = 'gu.id_group = 0';
-
-                $currentusers = $group->getusers();
-                if (!empty($currentusers)) $where[] = 'u.id NOT IN ('.implode(',',array_keys($currentusers)).')';
-            break;
-
-            case _SYSTEM_WORKSPACES :
-                // filtrage sur les groupes partagés
-                if (!empty($workspaces['list'][$workspaceid]['groups_shared'])) $where[] = 'gu.id_group IN ('.implode(',',array_keys($workspaces['list'][$workspaceid]['groups_shared'])).')';
-                else $where[] = "gu.id_group = 0";
-                
-                $currentusers = $workspace->getusers();
-                if (!empty($currentusers)) $where[] = 'u.id NOT IN ('.implode(',',array_keys($currentusers)).')';
-            break;
-
-        }
 
         $select =   "
                     SELECT      count(u.id) as nbuser
@@ -79,7 +82,9 @@ else
                     LEFT JOIN   ploopi_group_user gu
                     ON          gu.id_user = u.id
 
-                    WHERE       ".implode(' AND ', $where)."
+                    WHERE       (".implode(' AND ', $arrWhere).")
+                    
+                    OR          ISNULL(gu.id_group)
                     ";
 
         $db->query($select);
@@ -87,66 +92,42 @@ else
         if ($fields['nbuser'] < 25) $alphaTabItem = 99;
     }
 }
-
 ?>
-<div style="padding:4px;">
-    <?
-    $tabs_char = array();
+<div style="padding: 4px;"><?
+$tabs_char = array();
 
-    for($i=1;$i<27;$i++) $tabs_char[$i] = array('title' => chr($i+64), 'url' => "admin.php?alphaTabItem={$i}");
+for($i=1;$i<27;$i++) $tabs_char[$i] = array('title' => chr($i+64), 'url' => "admin.php?alphaTabItem={$i}");
 
-    $tabs_char[99] = array('title' => "&nbsp;tous&nbsp;", 'url' => "admin.php?alphaTabItem=99");
+$tabs_char[99] = array('title' => "&nbsp;tous&nbsp;", 'url' => "admin.php?alphaTabItem=99");
 
-    echo $skin->create_tabs($tabs_char,$alphaTabItem);
-    ?>
-</div>
+echo $skin->create_tabs($tabs_char,$alphaTabItem);
+?></div>
 
 <form action="<? echo ploopi_urlencode('admin.php'); ?>" method="post">
-<p class="ploopi_va" style="padding:4px;border-bottom:2px solid #c0c0c0;">
-    <span><? echo _SYSTEM_LABEL_USER; ?> :</span>
-    <input class="text" ID="system_user" name="pattern" type="text" size="15" maxlength="255" value="<? echo htmlentities($pattern); ?>">
-    <input type="submit" value="<? echo _PLOOPI_FILTER; ?>" class="button">
-    <input type="submit" name="reset" value="<? echo _PLOOPI_RESET; ?>" class="button">
-</p>
+<p class="ploopi_va"
+	style="padding: 4px; border-bottom: 2px solid #c0c0c0;"><span><? echo _SYSTEM_LABEL_USER; ?>
+:</span> <input class="text" ID="system_user" name="pattern" type="text"
+	size="15" maxlength="255" value="<? echo htmlentities($pattern); ?>"> <input
+	type="submit" value="<? echo _PLOOPI_FILTER; ?>" class="button"> <input
+	type="submit" name="reset" value="<? echo _PLOOPI_RESET; ?>"
+	class="button"></p>
 </form>
 
 <?
-$where = array();
+$strWhereName = '';
 
 if ($alphaTabItem == 99) // tous ou recherche
 {
     if ($pattern != '')
     {
         $pattern = $db->addslashes($pattern);
-        $where[] .=  "(u.lastname LIKE '%{$pattern}%' OR u.firstname LIKE '%{$pattern}%' OR u.login LIKE '%{$pattern}%')";
+        $strWhereName = " AND (u.lastname LIKE '%{$pattern}%' OR u.firstname LIKE '%{$pattern}%' OR u.login LIKE '%{$pattern}%')";
     }
 }
 else
 {
-    $where[] = "u.lastname LIKE '".chr($alphaTabItem+96)."%'";
+    $strWhereName = " AND u.lastname LIKE '".chr($alphaTabItem+96)."%'";
 }
-
-switch ($_SESSION['system']['level'])
-{
-    case _SYSTEM_GROUPS :
-        // filtrage sur les groupes partagés
-        if (!empty($groups['list'][$groupid]['groups_shared'])) $where[] = '( gu.id_group IN ('.implode(',',array_keys($groups['list'][$groupid]['groups_shared'])).') OR isnull(gu.id_group) )';
-        else $where[] = 'gu.id_group = 0';
-
-        $currentusers = $group->getusers();
-        if (!empty($currentusers)) $where[] = 'u.id NOT IN ('.implode(',',array_keys($currentusers)).')';
-    break;
-
-    case _SYSTEM_WORKSPACES :
-        // filtrage sur les groupes partagés
-        if (!empty($workspaces['list'][$workspaceid]['groups_shared'])) $where[] = 'gu.id_group IN ('.implode(',',array_keys($workspaces['list'][$workspaceid]['groups_shared'])).')';
-        else $where[] = "gu.id_group = 0";
-
-        $currentusers = $workspace->getusers();
-        if (!empty($currentusers)) $where[] = 'u.id NOT IN ('.implode(',',array_keys($currentusers)).')';
-    break;
-}
-
 
 $select =   "
             SELECT      u.id,
@@ -160,7 +141,11 @@ $select =   "
             LEFT JOIN   ploopi_group_user gu
             ON          gu.id_user = u.id
 
-            WHERE       ".implode(' AND ', $where)."
+            WHERE       ((".implode(' AND ', $arrWhere).")
+            
+            OR          ISNULL(gu.id_group))
+            
+            {$strWhereName}
 
             GROUP BY    u.id
             ";
@@ -207,17 +192,20 @@ $skin->display_array($columns, $values, 'array_userlist', array('sortable' => tr
 if ($_SESSION['system']['level'] == _SYSTEM_WORKSPACES)
 {
     ?>
-    <p class="ploopi_va" style="padding:4px;">
-        <span style="margin-right:5px;">Légende:</span>
-        <img src="<? echo $_SESSION['ploopi']['template_path']; ?>/img/system/adminlevels/level_user.png" />
-        <span style="margin-right:5px;"><? echo htmlentities($ploopi_system_levels[_PLOOPI_ID_LEVEL_USER]); ?></span>
-        <img src="<? echo $_SESSION['ploopi']['template_path']; ?>/img/system/adminlevels/level_groupmanager.png" />
-        <span style="margin-right:5px;"><? echo htmlentities($ploopi_system_levels[_PLOOPI_ID_LEVEL_GROUPMANAGER]); ?></span>
-        <img src="<? echo $_SESSION['ploopi']['template_path']; ?>/img/system/adminlevels/level_groupadmin.png" />
-        <span style="margin-right:5px;"><? echo htmlentities($ploopi_system_levels[_PLOOPI_ID_LEVEL_GROUPADMIN]); ?></span>
-        <img src="<? echo $_SESSION['ploopi']['template_path']; ?>/img/system/adminlevels/level_systemadmin.png" />
-        <span style="margin-right:5px;"><? echo htmlentities($ploopi_system_levels[_PLOOPI_ID_LEVEL_SYSTEMADMIN]); ?></span>
-    </p>
+<p class="ploopi_va" style="padding: 4px;"><span
+	style="margin-right: 5px;">Légende:</span> <img
+	src="<? echo $_SESSION['ploopi']['template_path']; ?>/img/system/adminlevels/level_user.png" />
+<span style="margin-right: 5px;"><? echo htmlentities($ploopi_system_levels[_PLOOPI_ID_LEVEL_USER]); ?></span>
+<img
+	src="<? echo $_SESSION['ploopi']['template_path']; ?>/img/system/adminlevels/level_groupmanager.png" />
+<span style="margin-right: 5px;"><? echo htmlentities($ploopi_system_levels[_PLOOPI_ID_LEVEL_GROUPMANAGER]); ?></span>
+<img
+	src="<? echo $_SESSION['ploopi']['template_path']; ?>/img/system/adminlevels/level_groupadmin.png" />
+<span style="margin-right: 5px;"><? echo htmlentities($ploopi_system_levels[_PLOOPI_ID_LEVEL_GROUPADMIN]); ?></span>
+<img
+	src="<? echo $_SESSION['ploopi']['template_path']; ?>/img/system/adminlevels/level_systemadmin.png" />
+<span style="margin-right: 5px;"><? echo htmlentities($ploopi_system_levels[_PLOOPI_ID_LEVEL_SYSTEMADMIN]); ?></span>
+</p>
     <?
 }
 ?>
