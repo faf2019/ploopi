83e6a01698077fc12f13b3c7f6004d3229c48385
Stéphane <stephane@ovensia.fr>
2009-06-15 07:38:27 +0000

    WebEdit : mise à jour du script SQL d'installation

--- install/webedit/structure.sql
+++ install/webedit/structure.sql
@@ -162,3 +162,6 @@ ALTER TABLE `ploopi_mod_webedit_article_draft` ADD `disabledfilter` TINYINT( 1 )
 ALTER TABLE `ploopi_mod_webedit_article` ADD `disabledfilter` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT '0';
 ALTER TABLE `ploopi_mod_webedit_article_draft` ADD `headcontent` LONGTEXT NOT NULL;
 ALTER TABLE `ploopi_mod_webedit_article` ADD `headcontent` LONGTEXT NOT NULL;
+
+ALTER TABLE `ploopi_mod_webedit_heading` ADD `private` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `visible` ;
+ALTER TABLE `ploopi_mod_webedit_heading` ADD `private_visible` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT '0' AFTER `private` ;
