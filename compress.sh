c8f5fb0aed9d6d832b88dfb3bd24152c90562dc9
Stéphane <stephane@ovensia.fr>
2009-09-16 12:45:54 +0000

    * Script cli : Correction d'un bug lors du chargement du gestionnaire d'erreur * Modification du script de compression (compress.sh)

--- compress.sh
+++ compress.sh
@@ -9,8 +9,10 @@ export YUIVER='-2.4.2'
 echo "COMPRESSION DU FICHIER functions.js"
 
 rm js/functions*.*
-cat js/*.js > js/functions.pack.js
-java -jar yuicompressor/build/yuicompressor$YUIVER.jar --charset ISO-8859-15 js/functions.pack.js | gzip > js/functions.pack.js.gz
+cat js/*.js > js/functions.js
+java -jar yuicompressor/build/yuicompressor$YUIVER.jar --charset ISO-8859-15 js/functions.js > js/functions.pack.js 
+gzip -c js/functions.pack.js > js/functions.pack.js.gz
+rm js/functions.js;
 
 for template in ./templates/backoffice/{eyeos,redmine,ploopi*}; do
     if [ -d $template ]; then
@@ -55,7 +57,7 @@ done
 for i in $( find ./lib/fckeditor \( -name '*.js' -or -name '*.css' \) -type f )
 do
     echo "Compression : $i => $i.gz"
-    cat $i | gzip > $i.gz
+    gzip -c $i > $i.gz
     export ta=`stat -c "%s" $i`
     export tb=`stat -c "%s" $i.gz`
     echo "Résultat : $ta => $tb"
