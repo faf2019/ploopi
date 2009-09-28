fee2fbeab2d07e71b842e07cb3adecf0130f5db5
Stéphane <stephane@ovensia.fr>
2009-09-28 16:06:42 +0000

    * Intégration du fichier de description de la métabase "système" * Script SQL de mise à jour vers 1.6.0.1

---
+++ install/system/update_ploopi_1.6.0.1.sql
@@ -0,0 +1,3 @@
+DELETE FROM `ploopi_param_default` WHERE name = 'system_set_cache';
+DELETE FROM `ploopi_param_type` WHERE name = 'system_set_cache';
+DELETE FROM `ploopi_param_choice` WHERE name = 'system_set_cache';
