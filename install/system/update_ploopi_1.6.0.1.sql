733b17d91389d346d8da92b652bf6e79c3036ab6
Stéphane <stephane@ovensia.fr>
2009-09-29 07:41:18 +0000

    Correction sur le fichier SQL de mise à jour vers 1.6.0.1 (manquait la mise à jour de version justement !)

--- install/system/update_ploopi_1.6.0.1.sql
+++ install/system/update_ploopi_1.6.0.1.sql
@@ -1,3 +1,6 @@
 DELETE FROM `ploopi_param_default` WHERE name = 'system_set_cache';
 DELETE FROM `ploopi_param_type` WHERE name = 'system_set_cache';
 DELETE FROM `ploopi_param_choice` WHERE name = 'system_set_cache';
+
+
+UPDATE `ploopi_module_type` SET `version` = '1.6.0.1', `author` = 'Ovensia', `date` = '20090929000000', `description` = 'Noyau du système' WHERE `ploopi_module_type`.`id` = 1;
