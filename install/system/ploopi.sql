b74c8cf89758ac4beefa80a5d05335b951003580
Stéphane <stephane@ovensia.fr>
2009-10-01 12:51:55 +0000

    Mise à jour du script SQL d'installation

--- install/system/ploopi.sql
+++ install/system/ploopi.sql
@@ -1003,9 +1003,9 @@ CHANGE `city` `city` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_swedish_
 CHANGE `country` `country` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
 CHANGE `floor` `floor` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
 CHANGE `office` `office` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-CHANGE `rank` `rank` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL 
+CHANGE `rank` `rank` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
 
 ALTER TABLE `ploopi_serializedvar` DROP PRIMARY KEY;
 ALTER TABLE `ploopi_serializedvar` ADD PRIMARY KEY ( `id` , `id_session` ) ;
 
-UPDATE `ploopi_module_type` SET `version` = '1.6.0.2', `author` = 'Ovensia', `date` = '20090930000000', `description` = 'Noyau du système' WHERE `ploopi_module_type`.`id` = 1;
+UPDATE `ploopi_module_type` SET `version` = '1.6.0.3', `author` = 'Ovensia', `date` = '20091001000000', `description` = 'Noyau du système' WHERE `ploopi_module_type`.`id` = 1;
