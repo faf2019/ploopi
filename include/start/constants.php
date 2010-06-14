14056771c5e2748ef014ae5f2b79dd1f47452145
Stéphane <stephane@ovensia.fr>
2010-06-14 10:39:06 +0000

    Initialisation de la version de développement 1.8 (1.7.9)

--- include/start/constants.php
+++ include/start/constants.php
@@ -31,7 +31,7 @@
  * @author Stéphane Escaich
  */
 
-define ('_PLOOPI_VERSION', '1.7.0.0');
+define ('_PLOOPI_VERSION', '1.7.9.0');
 define ('_PLOOPI_REVISION', '100614');
 
 define ('_PLOOPI_MSG_DBERROR',  'Database connection error, please contact administrator');
---
+++ install/system/update_ploopi_1.7.9.0.sql
@@ -0,0 +1,1 @@
+UPDATE `ploopi_module_type` SET `version` = '1.7.9.0', `author` = 'Ovensia', `date` = '20100614000000', `description` = 'Noyau du système' WHERE `ploopi_module_type`.`id` = 1;
