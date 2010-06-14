b4bfa6bab9f56fb53e2a67bba0646da3534ddc06
Stéphane <stephane@ovensia.fr>
2010-06-14 09:54:46 +0000

    Validation version stable 1.7.0.0

--- include/start/constants.php
+++ include/start/constants.php
@@ -31,8 +31,8 @@
  * @author Stéphane Escaich
  */
 
-define ('_PLOOPI_VERSION', '1.6.9.9.0');
-define ('_PLOOPI_REVISION', '100601');
+define ('_PLOOPI_VERSION', '1.7.0.0');
+define ('_PLOOPI_REVISION', '100614');
 
 define ('_PLOOPI_MSG_DBERROR',  'Database connection error, please contact administrator');
 define ('_SYSTEM_SYSTEMADMIN',  0);
