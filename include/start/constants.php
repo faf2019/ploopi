6b077954a270ff92c04e2e237280b323c4f6eb40
Stéphane <stephane@ovensia.fr>
2010-12-14 08:05:05 +0000

    Validation version 1.8

--- include/start/constants.php
+++ include/start/constants.php
@@ -31,8 +31,8 @@
  * @author Stéphane Escaich
  */
 
-define ('_PLOOPI_VERSION', '1.7.9.2');
-define ('_PLOOPI_REVISION', '100914');
+define ('_PLOOPI_VERSION', '1.8.0');
+define ('_PLOOPI_REVISION', '101214');
 
 define ('_PLOOPI_MSG_DBERROR',  'Database connection error, please contact administrator');
 define ('_SYSTEM_SYSTEMADMIN',  0);
---
+++ install/system/update_ploopi_1.8.0.sql
@@ -0,0 +1,2 @@
+UPDATE `ploopi_module_type` SET `version` = '1.8.0', `author` = 'Ovensia', `date` = '20101214000000', `description` = 'Noyau du système' WHERE `ploopi_module_type`.`id` = 1;
+
