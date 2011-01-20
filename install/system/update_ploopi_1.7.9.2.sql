c17d08aa7b6d1f65490c21c8758758c829b804ae
Stéphane <stephane@ovensia.fr>
2011-01-20 10:00:48 +0000

    Correction du charset sur les 2 derniers fichiers SQL de mise à jour du module "system" (UTF-8 => ISO-8859-15)

--- install/system/update_ploopi_1.7.9.2.sql
+++ install/system/update_ploopi_1.7.9.2.sql
@@ -1,2 +1,2 @@
-UPDATE `ploopi_module_type` SET `version` = '1.7.9.2', `author` = 'Ovensia', `date` = '20100914000000', `description` = 'Noyau du systÃ¨me' WHERE `ploopi_module_type`.`id` = 1;
+UPDATE `ploopi_module_type` SET `version` = '1.7.9.2', `author` = 'Ovensia', `date` = '20100914000000', `description` = 'Noyau du système' WHERE `ploopi_module_type`.`id` = 1;
 
--- install/system/update_ploopi_1.8.0.sql
+++ install/system/update_ploopi_1.8.0.sql
@@ -1,2 +1,2 @@
-UPDATE `ploopi_module_type` SET `version` = '1.8.0', `author` = 'Ovensia', `date` = '20101214000000', `description` = 'Noyau du systÃ¨me' WHERE `ploopi_module_type`.`id` = 1;
+UPDATE `ploopi_module_type` SET `version` = '1.8.0', `author` = 'Ovensia', `date` = '20101214000000', `description` = 'Noyau du système' WHERE `ploopi_module_type`.`id` = 1;
 
