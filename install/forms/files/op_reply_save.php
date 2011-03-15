7ac5dcabfbcec7e3aaa51be4ef9e0a49aaa5d4c7
Stéphane <stephane@ovensia.fr>
2011-03-15 10:25:53 +0000

    FORMS 3.1.8e
    * Correction d'un bug à l'enregistrement d'une réponse du formulaire (ploopi_die() résiduel)

--- install/forms/files/op_reply_save.php
+++ install/forms/files/op_reply_save.php
@@ -218,9 +218,6 @@ if (!empty($_GET['forms_id']) && is_numeric($_GET['forms_id']) && $objForm->open
          */
 
         ploopi_send_form($arrFrom, $arrTo, $arrEmailContent['Formulaire']['Titre'], $arrEmailContent);
-        ploopi_print_r($arrFrom);
-        ploopi_print_r($arrTo);
-        die();
     }
 
     if ($_SESSION['ploopi']['mode'] == 'backoffice') ploopi_redirect("admin.php?op=forms_viewreplies&forms_id={$objForm->fields['id']}");
