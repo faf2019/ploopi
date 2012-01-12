232c097045d33f44468a2d9d2c1a43456fdd3d35
Stéphane <stephane@ovensia.fr>
2012-01-12 18:15:17 +0000

    Ploopi 1.9.1.0
    * Validation version de dev

--- include/start/constants.php
+++ include/start/constants.php
@@ -31,8 +31,8 @@
  * @author Stéphane Escaich
  */
 
-define ('_PLOOPI_VERSION', '1.9.0');
-define ('_PLOOPI_REVISION', '111219');
+define ('_PLOOPI_VERSION', '1.9.1.0');
+define ('_PLOOPI_REVISION', '120112');
 
 define ('_PLOOPI_MSG_DBERROR',  'Database connection error, please contact administrator');
 define ('_SYSTEM_SYSTEMADMIN',  0);
