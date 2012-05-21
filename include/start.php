539fb97026d3e0001c020f27b0cdcee2d05d462b
Stéphane <stephane@ovensia.fr>
2012-05-21 11:55:08 +0000

    Correctif sur la gestion du module affiché par défaut.

--- include/start.php
+++ include/start.php
@@ -403,7 +403,7 @@ if ($_SESSION['ploopi']['mode'] == 'backoffice')
 
             if (!isset($ploopi_moduleid) && $_SESSION['ploopi']['backoffice']['moduleid'] == '' && !empty($_SESSION['ploopi']['workspaces'][$_SESSION['ploopi']['backoffice']['workspaceid']]['modules']))
             {
-                $arrModules = &$_SESSION['ploopi']['workspaces'][$_SESSION['ploopi']['backoffice']['workspaceid']]['modules'];
+                $arrModules = $_SESSION['ploopi']['workspaces'][$_SESSION['ploopi']['backoffice']['workspaceid']]['modules'];
                 $intAutoconnectModuleId = null;
 
                 foreach($arrModules as $intModuleId)
