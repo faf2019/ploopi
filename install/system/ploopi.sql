7afc817c983e05ecd85d6fe27d60d2a99baf50e6
Stéphane <stephane@ovensia.fr>
2013-03-18 17:40:18 +0000

    - Modification de la révision - Mise à jour des scripts d'installation et de mise à jour

--- install/system/ploopi.sql
+++ install/system/ploopi.sql
@@ -1892,7 +1892,7 @@ CREATE TABLE IF NOT EXISTS `ploopi_workspace_user_role` (
 --
 
 ALTER TABLE `ploopi_documents_file` ADD `md5id` VARCHAR( 32 ) NOT NULL AFTER `id` , ADD INDEX ( `md5id` );
-ALTER TABLE `ploopi_documents_folder` ADD `md5id` VARCHAR( 32 ) NOT NULL AFTER `id` , ADD INDEX ( `md5id` ); 
+ALTER TABLE `ploopi_documents_folder` ADD `md5id` VARCHAR( 32 ) NOT NULL AFTER `id` , ADD INDEX ( `md5id` );
 UPDATE `ploopi_documents_file` SET `md5id` = MD5(CONCAT(id, '_', timestp_create));
 UPDATE `ploopi_documents_folder` SET `md5id` = MD5(CONCAT(id, '_', timestp_create));
 
@@ -1901,4 +1901,32 @@ ALTER TABLE `ploopi_user` ADD FULLTEXT `ft` (`lastname` ,`firstname` ,`email` ,`
 
 INSERT INTO `ploopi_param_type` (`id_module_type`, `name`, `default_value`, `public`, `description`, `label`) VALUES ('1', 'system_user_required_fields', 'email,phone,service,function,city', '0', NULL, 'Champs requis dans le profil utilisateur');
 INSERT INTO `ploopi_param_default` (`id_module`, `name`, `value`, `id_module_type`) VALUES ('1', 'system_user_required_fields', 'email,phone,service,function,city', '1');
-UPDATE `ploopi_module_type` SET `version` = '1.9.3.0', `author` = 'Ovensia', `date` = '20121109000000', `description` = 'Noyau du système' WHERE `ploopi_module_type`.`id` = 1;
+
+ALTER TABLE `ploopi_module_workspace` ADD INDEX ( `id_module` ) ;
+ALTER TABLE `ploopi_module_workspace` ADD INDEX ( `id_workspace` ) ;
+ALTER TABLE `ploopi_module_type` ADD INDEX ( `label` ) ;
+ALTER TABLE `ploopi_group` ADD INDEX ( `parents` ) ;
+ALTER TABLE `ploopi_workspace_group` ADD INDEX ( `id_group` ) ;
+ALTER TABLE `ploopi_ticket_dest` ADD INDEX ( `id_user` ) ;
+ALTER TABLE `ploopi_ticket_watch` ADD INDEX ( `id_ticket` ) ;
+ALTER TABLE `ploopi_ticket_watch` ADD INDEX ( `id_user` ) ;
+ALTER TABLE `ploopi_ticket_watch` ADD INDEX ( `notify` ) ;
+
+INSERT INTO `ploopi_mimetype` (`ext`, `mimetype`, `filetype`, `group`) VALUES
+('docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'document', 'microsoft'),
+('dotx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.template', 'document', 'microsoft'),
+('xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'spreadsheet', 'microsoft'),
+('xltx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.template', 'spreadsheet', 'microsoft'),
+('xlam', 'application/vnd.ms-excel.addin.macroEnabled.12', 'spreadsheet', 'microsoft'),
+('xlsb', 'application/vnd.ms-excel.sheet.binary.macroEnabled.12', 'spreadsheet', 'microsoft'),
+('potx', 'application/vnd.openxmlformats-officedocument.presentationml.template', 'presentation', 'microsoft'),
+('ppsx', 'application/vnd.openxmlformats-officedocument.presentationml.slideshow', 'presentation', 'microsoft'),
+('pptx', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'presentation', 'microsoft'),
+('sldx', 'application/vnd.openxmlformats-officedocument.presentationml.slide', 'presentation', 'microsoft'),
+('ogv', 'video/ogg', 'video', 'video'),
+('oga', 'audio/ogg', 'audio', 'audio'),
+('webm', 'video/webm', 'video', 'video');
+
+UPDATE `ploopi_mimetype` SET `mimetype` = 'video/mp4' WHERE `ploopi_mimetype`.`ext` = 'm4v';
+
+UPDATE `ploopi_module_type` SET `version` = '1.9.3.0', `author` = 'Ovensia', `date` = '20130318000000', `description` = 'Noyau du système' WHERE `ploopi_module_type`.`id` = 1;
--- install/system/update_ploopi_1.9.3.0.sql
+++ install/system/update_ploopi_1.9.3.0.sql
@@ -1,1 +1,28 @@
-UPDATE `ploopi_module_type` SET `version` = '1.9.3.0', `author` = 'Ovensia', `date` = '20120413000000', `description` = 'Noyau du système' WHERE `ploopi_module_type`.`id` = 1;
+ALTER TABLE `ploopi_module_workspace` ADD INDEX ( `id_module` ) ;
+ALTER TABLE `ploopi_module_workspace` ADD INDEX ( `id_workspace` ) ;
+ALTER TABLE `ploopi_module_type` ADD INDEX ( `label` ) ;
+ALTER TABLE `ploopi_group` ADD INDEX ( `parents` ) ;
+ALTER TABLE `ploopi_workspace_group` ADD INDEX ( `id_group` ) ;
+ALTER TABLE `ploopi_ticket_dest` ADD INDEX ( `id_user` ) ;
+ALTER TABLE `ploopi_ticket_watch` ADD INDEX ( `id_ticket` ) ;
+ALTER TABLE `ploopi_ticket_watch` ADD INDEX ( `id_user` ) ;
+ALTER TABLE `ploopi_ticket_watch` ADD INDEX ( `notify` ) ;
+
+INSERT INTO `ploopi_mimetype` (`ext`, `mimetype`, `filetype`, `group`) VALUES
+('docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'document', 'microsoft'),
+('dotx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.template', 'document', 'microsoft'),
+('xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'spreadsheet', 'microsoft'),
+('xltx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.template', 'spreadsheet', 'microsoft'),
+('xlam', 'application/vnd.ms-excel.addin.macroEnabled.12', 'spreadsheet', 'microsoft'),
+('xlsb', 'application/vnd.ms-excel.sheet.binary.macroEnabled.12', 'spreadsheet', 'microsoft'),
+('potx', 'application/vnd.openxmlformats-officedocument.presentationml.template', 'presentation', 'microsoft'),
+('ppsx', 'application/vnd.openxmlformats-officedocument.presentationml.slideshow', 'presentation', 'microsoft'),
+('pptx', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'presentation', 'microsoft'),
+('sldx', 'application/vnd.openxmlformats-officedocument.presentationml.slide', 'presentation', 'microsoft'),
+('ogv', 'video/ogg', 'video', 'video'),
+('oga', 'audio/ogg', 'audio', 'audio'),
+('webm', 'video/webm', 'video', 'video');
+
+UPDATE `ploopi_mimetype` SET `mimetype` = 'video/mp4' WHERE `ploopi_mimetype`.`ext` = 'm4v';
+
+UPDATE `ploopi_module_type` SET `version` = '1.9.3.0', `author` = 'Ovensia', `date` = '20130318000000', `description` = 'Noyau du système' WHERE `ploopi_module_type`.`id` = 1;
