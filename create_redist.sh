4a96efd60de23c398c2367232b78214ae7a22733
Stéphane <stephane@ovensia.fr>
2019-12-05 15:14:36 +0000

    Ploopi 1.9.4.10

--- create_redist.sh
+++ create_redist.sh
@@ -3,7 +3,7 @@
 # Pour l'export SQL :
 # mysqldump -uroot -p<PASS> --opt --default-character-set=latin1 --comments=FALSE ploopixxxx > install/system/ploopi.sql
 
-export DEST=ploopi_1.9.4.9
+export DEST=ploopi_1.9.4.10
 
 #################################
 # compression des fichiers
---
+++ install/system/update_ploopi_1.9.4.10.sql
@@ -0,0 +1,1 @@
+UPDATE `ploopi_module_type` SET `version` = '1.9.4.10', `author` = 'Ovensia', `date` = '20191205000000', `description` = 'Noyau du système' WHERE `ploopi_module_type`.`id` = 1;
