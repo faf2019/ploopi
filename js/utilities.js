7451a55f60371836f35ff840dc5dca01e89025f1
Stéphane <stephane@ovensia.fr>
2020-02-25 18:09:20 +0000

    Ajout d'une fonction get_param au framework JS

--- js/utilities.js
+++ js/utilities.js
@@ -95,7 +95,18 @@ ploopi.execute_function = function(functionName, context) {
 
 
 
-
+ploopi.get_param = function(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+};
 
 
 
