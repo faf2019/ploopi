9774df5051d81816984534b685d2be64b11f191c
Stéphane <stephane@ovensia.fr>
2020-04-03 15:08:37 +0000

    BOOKING 1.3.3
    * Mise à jour de compatibilité pour PHP 7.3 / MariaDB 10.3

--- install/booking/structure.sql
+++ install/booking/structure.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 
--- 
+--
 -- Structure de la table `ploopi_mod_booking_event`
--- 
+--
 
 DROP TABLE IF EXISTS `ploopi_mod_booking_event`;
 CREATE TABLE IF NOT EXISTS `ploopi_mod_booking_event` (
@@ -25,9 +25,9 @@ CREATE TABLE IF NOT EXISTS `ploopi_mod_booking_event` (
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Structure de la table `ploopi_mod_booking_event_detail`
--- 
+--
 
 DROP TABLE IF EXISTS `ploopi_mod_booking_event_detail`;
 CREATE TABLE IF NOT EXISTS `ploopi_mod_booking_event_detail` (
@@ -46,9 +46,9 @@ CREATE TABLE IF NOT EXISTS `ploopi_mod_booking_event_detail` (
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Structure de la table `ploopi_mod_booking_resource`
--- 
+--
 
 DROP TABLE IF EXISTS `ploopi_mod_booking_resource`;
 CREATE TABLE IF NOT EXISTS `ploopi_mod_booking_resource` (
@@ -72,9 +72,9 @@ CREATE TABLE IF NOT EXISTS `ploopi_mod_booking_resource` (
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Structure de la table `ploopi_mod_booking_resource_workspace`
--- 
+--
 
 DROP TABLE IF EXISTS `ploopi_mod_booking_resource_workspace`;
 CREATE TABLE IF NOT EXISTS `ploopi_mod_booking_resource_workspace` (
@@ -86,9 +86,9 @@ CREATE TABLE IF NOT EXISTS `ploopi_mod_booking_resource_workspace` (
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Structure de la table `ploopi_mod_booking_resourcetype`
--- 
+--
 
 DROP TABLE IF EXISTS `ploopi_mod_booking_resourcetype`;
 CREATE TABLE IF NOT EXISTS `ploopi_mod_booking_resourcetype` (
@@ -130,3 +130,29 @@ CREATE TABLE IF NOT EXISTS `ploopi_mod_booking_event_subresource` (
   `id_subresource` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_event`,`id_subresource`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+UPDATE `ploopi_mod_booking_event_detail` SET `cancelreason` = '' WHERE ISNULL(`cancelreason`);
+ALTER TABLE `ploopi_mod_booking_event_detail` CHANGE `cancelreason` `cancelreason` varchar(255) NOT NULL DEFAULT ''  COMMENT '' ;
+UPDATE `ploopi_mod_booking_event_detail` SET `message` = '' WHERE ISNULL(`message`);
+ALTER TABLE `ploopi_mod_booking_event_detail` CHANGE `message` `message` text NOT NULL DEFAULT ''  COMMENT '' ;
+UPDATE `ploopi_mod_booking_event_detail` SET `emails` = '' WHERE ISNULL(`emails`);
+ALTER TABLE `ploopi_mod_booking_event_detail` CHANGE `emails` `emails` text NOT NULL DEFAULT ''  COMMENT '' ;
+UPDATE `ploopi_mod_booking_subresource` SET `name` = '' WHERE ISNULL(`name`);
+ALTER TABLE `ploopi_mod_booking_subresource` CHANGE `name` `name` varchar(255) NOT NULL DEFAULT ''  COMMENT '' ;
+UPDATE `ploopi_mod_booking_subresource` SET `reference` = '' WHERE ISNULL(`reference`);
+ALTER TABLE `ploopi_mod_booking_subresource` CHANGE `reference` `reference` varchar(255) NOT NULL DEFAULT ''  COMMENT '' ;
+UPDATE `ploopi_mod_booking_resourcetype` SET `name` = '' WHERE ISNULL(`name`);
+ALTER TABLE `ploopi_mod_booking_resourcetype` CHANGE `name` `name` varchar(255) NOT NULL DEFAULT ''  COMMENT '' ;
+UPDATE `ploopi_mod_booking_resource` SET `name` = '' WHERE ISNULL(`name`);
+ALTER TABLE `ploopi_mod_booking_resource` CHANGE `name` `name` varchar(255) NOT NULL DEFAULT ''  COMMENT '' ;
+UPDATE `ploopi_mod_booking_resource` SET `reference` = '' WHERE ISNULL(`reference`);
+ALTER TABLE `ploopi_mod_booking_resource` CHANGE `reference` `reference` varchar(255) NOT NULL DEFAULT ''  COMMENT '' ;
+UPDATE `ploopi_mod_booking_resource` SET `color` = '' WHERE ISNULL(`color`);
+ALTER TABLE `ploopi_mod_booking_resource` CHANGE `color` `color` varchar(16) NOT NULL DEFAULT ''  COMMENT '' ;
+UPDATE `ploopi_mod_booking_event` SET `object` = '' WHERE ISNULL(`object`);
+ALTER TABLE `ploopi_mod_booking_event` CHANGE `object` `object` varchar(255) NOT NULL DEFAULT ''  COMMENT '' ;
+UPDATE `ploopi_mod_booking_event` SET `periodicity` = '' WHERE ISNULL(`periodicity`);
+ALTER TABLE `ploopi_mod_booking_event` CHANGE `periodicity` `periodicity` varchar(16) NOT NULL DEFAULT ''  COMMENT '' ;
+UPDATE `ploopi_mod_booking_event` SET `comment` = '' WHERE ISNULL(`comment`);
+ALTER TABLE `ploopi_mod_booking_event` CHANGE `comment` `comment` mediumtext NOT NULL DEFAULT ''  COMMENT '' ;
