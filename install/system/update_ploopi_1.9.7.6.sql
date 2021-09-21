0e3fa04327f03fbe7bfca5ab501df74927997821
Stéphane <stephane@ovensia.fr>
2021-09-21 14:20:08 +0000

    Corrections sur valeur NULL non autorisée

--- install/system/update_ploopi_1.9.7.6.sql
+++ install/system/update_ploopi_1.9.7.6.sql
@@ -14,7 +14,7 @@ Cordialement,
 
 Ce message a été envoyé automatiquement. Nous vous remercions de ne pas répondre.';
 
-INSERT INTO `ploopi_param_type` (`id_module_type`, `name`, `default_value`, `public`, `description`, `label`) VALUES ('1', 'system_new_user_mail', '25', '0', NULL, 'Envoyer un courriel à la création d''un compte utilisateur');
+INSERT INTO `ploopi_param_type` (`id_module_type`, `name`, `default_value`, `public`, `description`, `label`) VALUES ('1', 'system_new_user_mail', '25', '0', '', 'Envoyer un courriel à la création d''un compte utilisateur');
 INSERT INTO `ploopi_param_default` (`id_module`, `name`, `value`, `id_module_type`) VALUES ('1', 'system_new_user_mail', '0', '1');
 INSERT INTO `ploopi_param_choice` (`id_module_type`, `name`, `value`, `displayed_value`) VALUES (1, 'system_new_user_mail', '0', 'non'), (1, 'system_new_user_mail', '1', 'oui');
 
