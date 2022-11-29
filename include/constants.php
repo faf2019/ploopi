6a77c0a6fa15dde573dd0d6e5fdac35e58690a3e
Stéphane <stephane@ovensia.fr>
2022-11-29 20:01:08 +0000

    Ploopi 1.9.7.8

--- include/constants.php
+++ include/constants.php
@@ -30,8 +30,8 @@
  * @author Ovensia
  */
 
-define ('_PLOOPI_VERSION', '1.9.7.7');
-define ('_PLOOPI_REVISION', '220315');
+define ('_PLOOPI_VERSION', '1.9.7.8');
+define ('_PLOOPI_REVISION', '221129');
 
 define ('_SYSTEM_SYSTEMADMIN',  0);
 define ('_SYSTEM_WORKSPACES',   'work');
---
+++ install/system/update_ploopi_1.9.7.8.sql
@@ -0,0 +1,1 @@
+UPDATE `ploopi_module_type` SET `version` = '1.9.7.8', `author` = 'Ovensia', `date` = '20221129000000', `description` = 'Noyau du système' WHERE `ploopi_module_type`.`id` = 1;
